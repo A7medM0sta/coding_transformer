54f55eec36114168749229d59b3a7b9e25bd559a
ahmed mustafa <Ahmed_Mustafa_1251@fci.helwan.edu.eg>
2024-08-26 20:26:35 +0300

    new changes

--- src/config.py
+++ src/config.py
@@ -20,7 +20,7 @@ def get_config():
 def get_weights_file_path(config, epoch: str):
     model_folder = f"{config['datasource']}_{config['model_folder']}"
     model_filename = f"{config['model_basename']}{epoch}.pt"
-    return str(Path('.') / model_folder / model_filename)
+    return str(Path('..') / model_folder / model_filename)
 
 # Find the latest weights file in the weights folder
 def latest_weights_file_path(config):
